ed457f5a472cadaf74c0d8b542ee2be27aa48c55
dcordobag <32066181+dcordobag@users.noreply.github.com>
2020-02-06 16:13:26 -0500

    cambios lista

--- WPFMultired/Resources/ImagesUrlResource.Designer.cs
+++ WPFMultired/Resources/ImagesUrlResource.Designer.cs
@@ -169,7 +169,7 @@ namespace WPFMultired.Resources {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a /Images/Others/img-bill.png.
+        ///   Busca una cadena traducida similar a /Images/Others/img-bill.jpg.
         /// </summary>
         internal static string ImgBill {
             get {
@@ -178,7 +178,7 @@ namespace WPFMultired.Resources {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a /Images/Others/img-coin.png.
+        ///   Busca una cadena traducida similar a /Images/Others/img-coin.jpg.
         /// </summary>
         internal static string ImgCoin {
             get {
