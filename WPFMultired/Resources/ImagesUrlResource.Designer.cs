1fe6401f1f9f89d93a8ca30e496c5171f67ce989
dcordobag <32066181+dcordobag@users.noreply.github.com>
2020-06-26 17:00:03 -0500

    cambios en multired fondos

--- WPFMultired/Resources/ImagesUrlResource.Designer.cs
+++ WPFMultired/Resources/ImagesUrlResource.Designer.cs
@@ -19,7 +19,7 @@ namespace WPFMultired.Resources {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ImagesUrlResource {
@@ -61,7 +61,7 @@ namespace WPFMultired.Resources {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Images/Backgrounds/bg-alert-blanck.png.
+        ///   Busca una cadena traducida similar a Images/Backgrounds/bg-alert-blanck.jpg.
         /// </summary>
         internal static string AlertBlanck {
             get {
