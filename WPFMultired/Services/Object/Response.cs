b76b2c0eee48cfafa23f33040c4914747e73903b
dcordobag <32066181+dcordobag@users.noreply.github.com>
2020-07-07 10:52:36 -0500

    cabios en ultima actualizacion

--- WPFMultired/Services/Object/Response.cs
+++ WPFMultired/Services/Object/Response.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using WPFMultired.Classes;
-using WPFMultired.Models;
 
 namespace WPFMultired.Services.Object
 {
